564b73b170b10cff51b5d7f151471e12687ef684
Egor Ostryakov <eg.ostr@mail.ru>
2021-05-22 22:47:12 +0300

    бля...

--- src/main/java/lab6/equation/Equations.java
+++ src/main/java/lab6/equation/Equations.java
@@ -10,8 +10,8 @@ import static java.lang.Math.pow;
 
 @Getter
 public enum Equations {
-    f1((x, y) -> y + (1 + x) * pow(y, 2), 1, -1,"y' = y + (1 + x)y^2, y(1) = -1", x -> {
-        if (x != 0) return 1 / x;
+    f1((x, y) -> y + (1 + x) * pow(y, 2), 1, -1, "y' = y + (1 + x)y^2, y(1) = -1", x -> {
+        if (x != 0) return -1 / x;
         throw new RuntimeException("Решение данного ОДУ не существует в точке x = 0");
     }),
     f2((x, y) -> y + (1 + x) * pow(y, 2), 1, -1, "y' = y + (1 + x)y^2, y(1) = -1", x -> {
@@ -28,7 +28,7 @@ public enum Equations {
     @Setter
     private double h;
 
-    Equations(BiFunction<Double, Double, Double> function,  double initPointArg, double initPointValue, String textView, Function<Double, Double> solution) {
+    Equations(BiFunction<Double, Double, Double> function, double initPointArg, double initPointValue, String textView, Function<Double, Double> solution) {
         this.function = function;
         this.initPointArg = initPointArg;
         this.left = initPointArg;
