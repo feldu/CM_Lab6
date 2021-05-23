46a0b27353cf7903290996947f530f489b74efe5
Egor Ostryakov <eg.ostr@mail.ru>
2021-05-23 22:41:28 +0300

    add equations

--- src/main/java/lab6/equation/Equations.java
+++ src/main/java/lab6/equation/Equations.java
@@ -6,18 +6,16 @@ import lombok.Setter;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-import static java.lang.Math.pow;
+import static java.lang.Math.*;
 
 @Getter
 public enum Equations {
-    f1((x, y) -> y + (1 + x) * pow(y, 2), 1, -1, "y' = y + (1 + x)y^2, y(1) = -1", x -> {
+    f1((x, y) -> y + (1 + x) * pow(y, 2), 1, -1, "y' = y + (1 + x)y^2, y(1) = -1;", x -> {
         if (x != 0) return -1 / x;
         throw new RuntimeException("Решение данного ОДУ не существует в точке x = 0");
     }),
-    f2((x, y) -> y + (1 + x) * pow(y, 2), 1, -1, "y' = y + (1 + x)y^2, y(1) = -1", x -> {
-        if (x != 0) return 1 / x;
-        throw new RuntimeException("Решение данного ОДУ не существует в точке x = 0");
-    });
+    f2((x, y) -> pow((x - y), 2) + 1, 0, 0, "y' = (x - y)^2 + 1, y(0) = 0;", x -> x),
+    f3((x, y) -> x * Math.exp(-pow(x, 2)) - 2 * x * y, -1, 1 / (2 * E), "y' = xe^(-x)^2 - 2xy, y(-1) = 1 / 2E;", x -> pow(x, 2) / 2 * exp(-pow(x, 2)));
     private BiFunction<Double, Double, Double> function;
     private Function<Double, Double> solution;
     private String textView;
